23d80b9a643f7acbce77dc354dc45acd14652fcc
Dominik Toczek <dominiktoczek@interia.pl>
2018-12-10 09:21:56 +0100

    Added moving marker function

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
+// Funkcja tworząca mapę oraz znacznik
 function initMap() {
-    let position = {lat: 50.05, lng: 19.95};
+    let position = {lat: lat, lng: lng};
     let map = document.getElementById('map');
 
-    let googleMap = new google.maps.Map(map, {zoom: 10, center: position});
+    let googleMap = new google.maps.Map(map, {zoom: 5, center: position});
     marker = new google.maps.Marker({position: position, map: googleMap});
 }
+
+let lat = 50.05;
+let lng = 19.95;
+// Funkcja zmieniająca pozycję znacznika mapy za pomocą "strzałek" na klawiaturze
+document.addEventListener('keydown', (e) => {
+    switch(e.keyCode)
+    {
+        case 38:
+            lat += 0.05;
+            break;
+        case 40:
+            lat -= 0.05;
+            break;
+        case 37:
+            lng -= 0.05;
+            break;
+        case 39:
+            lng += 0.05;
+            break;
+    }
+
+    marker.setPosition({lat: lat, lng: lng});
+})
